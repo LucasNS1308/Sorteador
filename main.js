38c9262effc245c299f81df3fc7fbd0b216a3454
Lucas Nascimento <lucas123crc@gmail.com>
2026-02-20 00:27:15 -0300

    validando os numeros do input

--- main.js
+++ main.js
@@ -1,19 +1,37 @@
-const numberMin = document.getElementById('min');
-const numberMax = document.getElementById('max');
-const quantity = document.getElementById('quantity');
-const btnSortear = document.getElementById('btn');
+const numberMin = document.getElementById("min");
+const numberMax = document.getElementById("max");
+const quantity = document.getElementById("quantity");
+const btnSortear = document.getElementById("btn");
 const regex = /\D/g;
 
-numberMin.addEventListener('input', () => {
-  numberMin.value = numberMin.value.replace(regex, '');
+numberMin.addEventListener("input", () => {
+  numberMin.value = numberMin.value.replace(regex, "");
+  numberMax.value = numberMax.value.replace(regex, "");
+  quantity.value = quantity.value.replace(regex, "");
 });
 
-btnSortear.addEventListener('click', (event) => {
+btnSortear.addEventListener("click", (event) => {
   event.preventDefault();
   let valueSorteador = quantity.value.trim();
+  let minValue = numberMin.value.trim();
+  let maxValue = numberMax.value.trim();
 
-  if (valueSorteador === '') {
-    alert('Insira um número válido');
+  if (valueSorteador === "") {
+    alert("Insira a quantidade de números a serem sorteados");
+  } else if (valueSorteador <= 0) {
+    alert("Insira um número maior que 0 para o sorteio");
+    return;
+  } else if (minValue === "") {
+    alert("Insira um valor mínimo para o sorteio");
+    return;
+  } else if (maxValue === "") {
+    alert("Insira um valor máximo para o sorteio");
+    return;
+  } else if (maxValue <= 10) {
+    alert("Insira um valor máximo maior que 10 para o sorteio");
+    return;
+  } else if (minValue >= maxValue) {
+    alert("O valor mínimo deve ser menor que o valor máximo para o sorteio");
     return;
   }
 });
